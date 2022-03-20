d74ba37e9f044bc6c831748e87bb26f06f5608d7
woog2roid <wooguijung@korea.ac.kr>
2022-03-20 11:43:42 +0000

    setup: main.ts 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import ormconfig from 'ormconfig';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot(ormconfig),
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors({
+    origin: [
+      'https://woog2roid.github.io',
+      'https://service.woog2roid.dev',
+      'https://todo.woog2roid.dev',
+      //goorm dev 환경
+      'https://mil-frontend-tcuwk.run.goorm.io',
+    ],
+    credentials: true,
+  });
+
+  app.use(cookieParser());
+
+  const config = new DocumentBuilder().setTitle('Playground API').build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
+  console.log(`server listening on port ${PORT}`);
 }
 bootstrap();
