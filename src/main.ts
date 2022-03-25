b5587e029e394279192ec5e499030b04c7fb3634
woog2roid <wooguijung@korea.ac.kr>
2022-03-25 14:38:03 +0000

    setup: hot reloading devDependency 추가

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,51 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import cookieParser from 'cookie-parser';
+import passport from 'passport';
+import session from 'express-session';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './http-exception.filter';
+
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
 
   app.enableCors({
     origin: [
       'https://woog2roid.github.io',
       'https://service.woog2roid.dev',
       'https://todo.woog2roid.dev',
-      //goorm dev 환경
       'https://mil-frontend-tcuwk.run.goorm.io',
     ],
     credentials: true,
   });
 
   app.use(cookieParser());
+  app.use(
+    session({
+      resave: false,
+      saveUninitialized: false,
+      secret: process.env.SESSION_COOKIE_SECRET,
+      cookie: {
+        httpOnly: true,
+      },
+    }),
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   const config = new DocumentBuilder().setTitle('Playground API').build();
   const document = SwaggerModule.createDocument(app, config);
