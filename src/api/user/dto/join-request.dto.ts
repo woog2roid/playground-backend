647aac97c474ef32c87d1f57930f923345d76fe4
woog2roid <wooguijung@korea.ac.kr>
2022-04-29 15:59:22 +0000

    feat: user DTO에 api property와 validation 추가

--- src/api/user/dto/join-request.dto.ts
+++ src/api/user/dto/join-request.dto.ts
@@ -1,8 +1,33 @@
-import { PickType } from '@nestjs/swagger';
+import { ApiProperty, PickType } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 import { Users } from '../../../entities/Users.entity';
 
 export class JoinRequestDto extends PickType(Users, [
   'id',
   'password',
   'nickname',
-] as const) {}
+] as const) {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    example: 'test12345',
+    description: '아이디',
+  })
+  public id: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    example: '**********',
+    description: '비밀번호',
+  })
+  public password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    example: '테스트 계정',
+    description: '닉네임',
+  })
+  public nickname: string;
+}
